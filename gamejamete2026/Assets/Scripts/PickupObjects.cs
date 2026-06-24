a7d7072e2832838db269c9518c410e609b8e0bd9
James Kalekas <95308100+sdeadfowl@users.noreply.github.com>
2026-06-24 10:34:11 -0400

    Hold objects
    Début création hold object, pour ensuite permettre de les lancer et après  permettre de faire que les ennemies les lance, mais pour l'instant ca permet seulement de tenir

--- gamejamete2026/Assets/Scripts/PickupObjects.cs
+++ gamejamete2026/Assets/Scripts/PickupObjects.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PickupObjects : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    bool isHoldingObject = false;
+    
+    TempParent tempParent;
+    Rigidbody rb;
+
+    Vector3 objectPos;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        tempParent = TempParent.Instance;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(isHoldingObject)
+        {
+            Hold();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        //pickup
+        if (tempParent != null)
+        {
+            isHoldingObject = true;
+            rb.useGravity = false;
+            rb.detectCollisions = true;
+
+            this.transform.SetParent(tempParent.transform);
+        }
+        else
+        {
+            Debug.Log("Temp Parent not in scene");
+        }
+    }
+    private void OnMouseUp()
+    {
+        //drop
+        isHoldingObject = false;
+        rb.useGravity = true;
+        rb.detectCollisions = true;
+        this.transform.SetParent(null);
+    }
+    private void OnMouseExit()
+    {
+        //drop
+        isHoldingObject = false;
+        rb.useGravity = true;
+        rb.detectCollisions = true;
+        this.transform.SetParent(null);
+    }
+    private void Hold()
+    {
+        objectPos = tempParent.transform.position;
+        this.transform.position = objectPos;
     }
 }
